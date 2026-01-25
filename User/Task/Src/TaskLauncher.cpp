cd130c29b66d343658bacd2e5468c5239de9e5ea
Aeskyrr17 <kiera171717zyt@gmail.com>
2026-01-25 17:13:23 +0800

    add TaskLauncher FSM & add dart_config

--- User/Task/Src/TaskLauncher.cpp
+++ User/Task/Src/TaskLauncher.cpp
@@ -10,24 +10,129 @@
 TX_THREAD LauncherThread;
 uint8_t LauncherThreadStack[2048] = {0};
 
+Launcher_Context_t dart;
+Loading_State loading_state;
+msg_cmd_t cmd{};
+msg_motor_ctrl_t msg_motorctrl{};
+msg_sensors_t sensors{};
+msg_launcher_status_t msg_launcher_status{};
+// tof_data_t tof{};
+
 [[nonreturn]] void LauncherThreadFun(ULONG initial_input) 
 {
     UNUSED(initial_input); 
 
     om_topic_t *motorctrl_topic = om_config_topic(nullptr, "ca", "motorctrl", sizeof(msg_motor_ctrl_t));
-    msg_motor_ctrl_t motorctrl{};
+    om_topic_t *launcherstatus_topic = om_config_topic(nullptr, "ca", "launcherstatus", sizeof(msg_launcher_status_t));
 
     om_suber_t *cmd_suber = om_subscribe(om_find_topic("cmd", UINT32_MAX));
-    msg_cmd_t cmd{};
     om_suber_t *ins_suber = om_subscribe(om_find_topic("ins", UINT32_MAX));
-    msg_ins_t ins{};
-    
+    // om_suber_t *tof_suber = om_subscribe(om_find_topic("tof", UINT32_MAX));
+
+    //初始化
+    Launcher_Init();
+
     for (;;) 
     {
         om_suber_export(cmd_suber, &cmd, false);
+        // om_suber_export(tof_suber, &tof, false);
 
-        om_publish(motorctrl_topic, &motorctrl, sizeof(msg_motor_ctrl_t), true, false);
+        //yaw轴控制
+        if (dart.current_state != IDLE )
+        {
+            msg_motorctrl.target_yaw = cmd.final_target_yaw;
+        }
+        else 
+        {
+            msg_motorctrl.target_yaw = 0;
+        }
+        
+        //FSM   
+        switch (dart.current_state)
+        {
+            case IDLE:
+                msg_motorctrl.yaw_speed = 0.0f;
+                msg_motorctrl.Coil_speed = 0.0f;
+                msg_motorctrl.trigger_lock = true;
+
+                // 收到prepare
+                if (cmd.launcher_action == DART_PREPARE) 
+                    dart.current_state = RETRACTING;
+                break;
+
+            case RETRACTING:
+                msg_motorctrl.Coil_speed = 10.0f;//todo:注意正负号
+
+                if (sensors.is_coil_reset)
+                    msg_motorctrl.trigger_lock = true;
+                    dart.current_state = LOCKED;
+                break;
+
+            case LOCKED:
+                msg_motorctrl.Coil_speed = 0.0f;
+                if (cmd.launcher_action == DART_PREPARE)
+                {
+                    dart.current_state = LOADING;
+                }
+                break;
+
+            case LOADING:
+                loading_state.ready_for_loading = true;
+                if (loading_state.load_is_done)
+                {
+                    loading_state.ready_for_loading = false;
+                    dart.current_state = TENSIONING;
+                }
+                break;
+
+
+            case TENSIONING:
+                msg_motorctrl.String_target_force = cmd.final_target_tension;
+                if (sensors.is_string_tight)
+                    dart.current_state = TENSIONED;
+                break;
+
+            case TENSIONED:
+                msg_motorctrl.String_target_force = cmd.final_target_tension;//保持力矩
+
+                if (cmd.launcher_action == DART_FIRE)
+                {
+                    if (sensors.is_door_open)
+                        dart.current_state = FIRING;
+                }
+                if (!sensors.is_string_tight)
+                {
+                    dart.current_state = TENSIONING;
+                }
+                break;
+
+            case FIRING:
+                msg_motorctrl.trigger_lock = false;
+                //todo:添加发射完成逻辑
+                if (sensors.fire_done)
+                {
+                    msg_launcher_status.is_fire_finished = true;
+                }
+                break;
+        }
+
+
+
+        om_publish(motorctrl_topic, &msg_motorctrl, sizeof(msg_motor_ctrl_t), true, false);
+        om_publish(launcherstatus_topic, &msg_launcher_status,sizeof(msg_launcher_status_t), true, false);
         tx_thread_sleep(1);
     }
+
+}
+//todo:完善init
+void Launcher_Init()
+{
+    msg_motorctrl.yaw_mode = SPD;
+    msg_motorctrl.Coil_mode = SPD;
+    msg_motorctrl.Coil_speed = 0.0f;
+    msg_motorctrl.yaw_speed = 0.0f;
+    msg_motorctrl.trigger_lock = true;
+    msg_launcher_status.is_fire_finished = false;
+
 
 }
--- User/Task/Src/TaskSysCtrl.cpp
+++ User/Task/Src/TaskSysCtrl.cpp
@@ -3,34 +3,60 @@
 
 #include "om.h"
 #include "magicmsgs.hpp"
-#include "config_launcher.hpp"
+#include "TaskSysCtrl.hpp"
 
 TX_THREAD SysctrlThread;
 uint8_t SysctrlThreadStack[2048] = {0};
 
-__attribute__((section(".RAM_D1"))) uint8_t tof_rx[TOF_DATA_SIZE];
+// __attribute__((section(".RAM_D1"))) uint8_t tof_rx[TOF_DATA_SIZE];
 
 void Run_Hand_Control(msg_remoter_t* remoter, msg_cmd_t* cmd);
 void Run_Auto_Control();
+void Update_Dart_Index();
+
+msg_cmd_t msg_cmd{};
+msg_remoter_t remoter{};
+msg_launcher_status_t launcher_status{};
+
+
+//配置各飞镖offset及torque
+Dart_Config_t dart_config[10] = {
+{0.0f,0.0f}, //不使用0号
+{0.0f,0.0f},
+{0.0f,0.0f},
+{0.0f,0.0f},
+{0.0f,0.0f},
+{0.0f,0.0f},
+{0.0f,0.0f},
+};  
+
+SysCtrl_Context_t dart_context = {
+    0,0,
+    {1,2,3,4}
+};
 
 [[nonreturn]] void SysctrlThreadFun(ULONG initial_input) 
 {
     UNUSED(initial_input); 
 
     om_topic_t *cmd_topic = om_config_topic(nullptr, "ca", "cmd", sizeof(msg_cmd_t));
-    msg_cmd_t cmd{};
 
     om_suber_t *remoter_suber = om_subscribe(om_find_topic("remoter", UINT32_MAX));
-    msg_remoter_t remoter{};
+    om_suber_t *launcherstatus_suber = om_subscribe(om_find_topic("launcherstatus",UINT32_MAX));
+
     // om_suber_t *ins_suber = om_subscribe(om_find_topic("ins", UINT32_MAX));
     // msg_ins_t ins{};
+    // om_suber_t *launcher_suber = om_subscribe(om_find_topic("launcher", UINT32_MAX))
+
+
 
 
     for (;;)
     {   
-        memset(&cmd, 0, sizeof(msg_cmd_t));//每次循环清空cmd
+        memset(&msg_cmd, 0, sizeof(msg_cmd_t));//每次循环清空cmd
 
         om_suber_export(remoter_suber, &remoter, false);
+        om_suber_export(launcherstatus_suber,&launcher_status,false);
         // om_suber_export(ins_suber, &ins, false);
 
 
@@ -52,6 +78,9 @@ void Run_Auto_Control();
         // float tof_distance = static_cast<float>(tof_raw->distance) * 1.0f; // cm
         // float tof_temp = static_cast<float>(tof_raw->temp_raw) / 8.0f - 256.0f; // °C
 
+        Update_Dart_Index();
+
+
         //todo:切换手控模式与自动模式。还没有写龙门架相关.
         //todo:确定遥控器逻辑，考虑一下归位/上弦/发射的逻辑
         /*remoter：左下or右下-relax；左中-handcontrol；左上&右上-auto
@@ -60,24 +89,25 @@ void Run_Auto_Control();
 
         if (remoter.left_sw == Down)
         {
-            cmd.launcher_action = DART_RELAX;
+            msg_cmd.launcher_action = DART_RELAX;
         }
         else if (remoter.left_sw == Mid)
         {
-            Run_Hand_Control(&remoter, &cmd);
+            Run_Hand_Control(&remoter, &msg_cmd);
         }
         else if (remoter.left_sw == Up && remoter.right_sw == Up)
         {
             Run_Auto_Control();
-            cmd.launcher_action = DART_RELAX; //todo:默认不发射，还未确定auto逻辑
+            msg_cmd.launcher_action = DART_RELAX; //todo:默认不发射，还未确定auto逻辑
         }
         else 
         {
-            cmd.launcher_action = DART_RELAX;
+            msg_cmd.launcher_action = DART_RELAX;
         }
-        om_publish(cmd_topic, &cmd, sizeof(msg_cmd_t), true, false);
+        om_publish(cmd_topic, &msg_cmd, sizeof(msg_cmd_t), true, false);
         tx_thread_sleep(1);
     }
+    
 }
 
 
@@ -90,18 +120,29 @@ void Run_Auto_Control();
  */
 void Run_Hand_Control(msg_remoter_t* remoter, msg_cmd_t* cmd)
 {
+    int shot_index = dart_context.current_dart_index;
+    int dart_id = dart_context.load_sequence[shot_index];
+
+    float my_offset = dart_config[dart_id].yaw_offset;
+    float my_tension = dart_config[dart_id].tension_torque;
+
+
     if (remoter->right_sw == Down)
     {
-        cmd->launcher_action = DART_TENSION;
-        cmd->tension_value = 0.1f; //todo:手动设定一个默认值，后续可通过遥控器调节?还是使用tension_set？还是用啥东西来计算？
+        cmd->launcher_action = DART_RELAX;
     }
     else if (remoter->right_sw == Mid)
     {
-        cmd->launcher_action = DART_RETRACT;
+        cmd->launcher_action = DART_PREPARE;
+        cmd->final_target_yaw = remoter->right_x * 100 + my_offset;  //todo:确定摇杆灵敏度,不确定手控模式需不需要加上offset。
+        cmd->final_target_tension = my_tension;
+
     }
     else if (remoter->right_sw == Up)
     {
         cmd->launcher_action = DART_FIRE;
+        cmd->final_target_yaw = (remoter->right_x * 100.0f) + my_offset; //fire模式下yaw角度和torque都需要保持
+        cmd->final_target_tension = my_tension;
     }
     else 
     {
@@ -115,5 +156,26 @@ void Run_Hand_Control(msg_remoter_t* remoter, msg_cmd_t* cmd)
  */
 void Run_Auto_Control()
 {
+
+}
+
+/**
+ * @brief 更新dart_id,利用上升沿判断上一阶段是否开火
+ * 
+ */
+void Update_Dart_Index()
+{
+    if (launcher_status.is_fire_finished && !dart_context.last_fire_status)
+    {
+        dart_context.current_dart_index++;
+    }
+    dart_context.last_fire_status = launcher_status.is_fire_finished;//更新状态
+}
+
+
+
+void Find_Dart_Speed()
+{
+
 
 }
